2e39913e0af07e225263b7dc8198f6e0c43ed060
Hasan Malik <hasanimranmalik245@gmail.com>
2025-10-20 16:30:33 -0400

    3 more files

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -24,12 +24,38 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
-        // TODO Task 1: Complete this method based on its provided documentation
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
+        // Task 1: Complete this method based on its provided documentation
         //      and the documentation for the dog.ceo API. You may find it helpful
         //      to refer to the examples of using OkHttpClient from the last lab,
         //      as well as the code for parsing JSON responses.
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+
+        try {
+            Request req = new Request.Builder().url("https://dog.ceo/api/breed/" + breed + "/list").build();
+            Response res = client.newCall(req).execute();
+            String output = res.body().string();
+            JSONObject json = new JSONObject(output);
+
+
+            if (json.get("message").toString() == "Breed not found (main breed does not exist)") {
+                throw new BreedNotFoundException(breed);
+            }
+
+            JSONArray subBreeds = json.getJSONArray("message");
+            ArrayList<String> ans = new ArrayList<String>();
+
+
+            // note: enhanced-for doesn't work on JSONArray
+            for (int i = 0; i < subBreeds.length(); i++) {
+                ans.add(subBreeds.get(i).toString());
+            }
+
+            return ans;
+
+        } catch (Exception e) {
+            throw new BreedNotFoundException(breed);
+        }
+
     }
+
 }
--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -1,10 +1,53 @@
 package dogapi;
 
+import java.sql.SQLOutput;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+
+//         MY TESTS:
+//        String hasansBreed = "corgi";
+//        DogApiBreedFetcher thing = new DogApiBreedFetcher();
+//        try {
+//            System.out.println("HEY");
+//            System.out.println(thing.getSubBreeds(hasansBreed));
+//        } catch (Exception e) {
+//            System.out.println("Breed not found: " + hasansBreed);
+//        }
+
+
+//        CachingBreedFetcher thing2 = new CachingBreedFetcher(new DogApiBreedFetcher());
+//        String answer = "";
+//        try {
+//            answer = thing2.getSubBreeds(hasansBreed).toString();
+//        } catch (Exception e) {
+//            System.out.println("Breed not found: " + hasansBreed);
+//        }
+//
+//        System.out.println("here: " + answer);
+//        System.out.println(thing2.getCallsMade());
+//
+//        try {
+//            answer = thing2.getSubBreeds("hound").toString();
+//        } catch (Exception e) {
+//            System.out.println("Breed not found: " + "hound");
+//        }
+//
+//        System.out.println(answer + " " + thing2.getCallsMade());
+//
+//        try {
+//            answer = thing2.getSubBreeds("corgi").toString();
+//        } catch (Exception e) {
+//            System.out.println("Breed not found: " + "corgi");
+//        }
+//
+//        System.out.println(answer + " " + thing2.getCallsMade());
+
+
+
+
         String breed = "hound";
         BreedFetcher breedFetcher = new CachingBreedFetcher(new BreedFetcherForLocalTesting());
         int result = getNumberOfSubBreeds(breed, breedFetcher);
@@ -24,8 +67,17 @@ public class Main {
      * returned by the fetcher
      */
     public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
-        // TODO Task 3 implement this code so that it is entirely consistent with its provided documentation.
+        // Task 3 implement this code so that it is entirely consistent with its provided documentation.
         // return statement included so that the starter code can compile and run.
-        return -1;
+
+        Integer answer = 0;
+
+        try {
+            answer = breedFetcher.getSubBreeds(breed).size();
+        } finally {
+            return answer;
+        }
+
+
     }
 }
