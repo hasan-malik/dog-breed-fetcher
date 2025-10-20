4d98040f71d340eab104338662af4d05ebb3ed2f
Hasan Malik <hasanimranmalik245@gmail.com>
2025-10-20 16:30:04 -0400

    first 3 files

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -1,5 +1,11 @@
 package dogapi;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.*;
 
 /**
@@ -13,16 +19,58 @@ import java.util.*;
  * The cache maps the name of a breed to its list of sub breed names.
  */
 public class CachingBreedFetcher implements BreedFetcher {
-    // TODO Task 2: Complete this class
+    private final OkHttpClient client = new OkHttpClient();
+    private HashMap<String, List<String>> Data = new HashMap<>();
+    private BreedFetcher providedFetcher;
     private int callsMade = 0;
-    public CachingBreedFetcher(BreedFetcher fetcher) {
 
+    public CachingBreedFetcher(BreedFetcher fetcher) {
+        providedFetcher = fetcher;
     }
 
     @Override
-    public List<String> getSubBreeds(String breed) {
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
+
+
+        if (Data.containsKey(breed)) {
+            return Data.get(breed);
+        }
+
+        try {
+            callsMade++;
+            List<String> answer = providedFetcher.getSubBreeds(breed);
+            Data.put(breed, answer);
+
+            return answer;
+
+
+//            Request req = new Request.Builder().url("https://dog.ceo/api/breed/" + breed + "/list").build();
+//            Response res = client.newCall(req).execute();
+//            String output = res.body().string();
+//            JSONObject json = new JSONObject(output);
+//
+//
+//            if (json.get("message").toString() == "Breed not found (main breed does not exist)") {
+//                throw new BreedNotFoundException(breed);
+//            }
+//
+//            JSONArray subBreeds = json.getJSONArray("message");
+//            ArrayList<String> ans = new ArrayList<String>();
+//
+//            // note: enhanced-for doesn't work on JSONArray
+//            for (int i = 0; i < subBreeds.length(); i++) {
+//                ans.add(subBreeds.get(i).toString());
+//            }
+//
+//            Data.put(breed, ans);
+//            callsMade++;
+//
+//            return ans;
+
+        } catch (Exception e) {
+            throw new BreedNotFoundException(breed);
+        }
+
     }
 
     public int getCallsMade() {
